a87a2beeb9baa0b93dbe4dd393aab68e3d91ffdb
Rayan Encarnacion <rayanbalbuena@gmail.com>
2022-01-24 22:19:34 -0400

    Framer Motion variants for animations

---
+++ src/helpers/MyMotionVariants.js
@@ -0,0 +1,8 @@
+export const searchBarVariant = {
+  hidden: { x: "105%" },
+  show: { x: ["105%", "-3%", "3%", "0%"], transition: { duration: 0.8 } },
+};
+export const cartVariant = {
+  hidden: { x: "105%" },
+  show: { x: 0, transition: { duration: 0.6 } },
+};
