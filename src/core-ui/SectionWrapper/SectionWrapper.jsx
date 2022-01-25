1740c7dbfbc4cb39fad25709c71c3e140cf5b897
Rayan Encarnacion <rayanbalbuena@gmail.com>
2022-01-24 22:20:41 -0400

    Section wrapper component added

---
+++ src/core-ui/SectionWrapper/SectionWrapper.jsx
@@ -0,0 +1,7 @@
+import StyledSectionWrapper from "./StyledSectionWrapper";
+
+const SectionWrapper = ({ children }) => {
+  return <StyledSectionWrapper>{children}</StyledSectionWrapper>;
+};
+
+export default SectionWrapper;
---
+++ src/core-ui/SectionWrapper/StyledSectionWrapper.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export default styled.section`
+  padding-bottom: 3.5rem;
+  padding-top: 3.5rem;
+  padding-left: 1.5em;
+  padding-right: 1.5em;
+`;
