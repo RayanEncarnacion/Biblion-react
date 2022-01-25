e406ad81daee3157565ae9490aab2f99311b2dcb
Rayan Encarnacion <rayanbalbuena@gmail.com>
2022-01-24 22:24:51 -0400

    core-ui index exports added

---
+++ src/core-ui/index.js
@@ -0,0 +1,6 @@
+import Button from "./Button/Button";
+import LayoutContainer from "./LayoutContainer/LayoutContainer";
+import SectionWrapper from "./SectionWrapper/SectionWrapper";
+import SectionTitle from "./SectionTitle/SectionTitle";
+
+export { Button, LayoutContainer, SectionWrapper, SectionTitle };
